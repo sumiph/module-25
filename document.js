9788ee6c9dae205a1f14faf47b86c7ee06fbd9d1
unknown <sumi63939@gmail.com>
2023-08-15 05:46:57 -0700

    addad

--- document.js
+++ document.js
@@ -3,12 +3,12 @@ document.getElementById('btn').addEventListener('click',function(){
     const outputEmail= emailFiled.value;
     const getpassword = document.getElementById('password');
     const outputPassword = getpassword.value;
-    if(outputEmail=== 'sokhiakter106@gmail.com' && outputPassword==='sumi123'){
-        window.location.href='bank.html';
-    }
-    else{
-        alert('emali or passwor are not difiend');
-    }
+    // if(outputEmail=== 'sokhiakter106@gmail.com' && outputPassword==='sumi123'){
+        // window.location.href='bank.html';
+    // }
+    // else{
+        // alert('emali or passwor are not difiend');
+    // }
     emailFiled.value='';
 })
 
